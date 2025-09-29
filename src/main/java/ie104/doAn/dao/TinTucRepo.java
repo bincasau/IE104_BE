ca46b09cffdb216374f00160ee568f990ffe9e11
bincasau <phinew331@gmail.com>
2025-09-29 22:00:14 +0700

    bo sung tint tuc

--- src/main/java/ie104/doAn/dao/TinTucRepo.java
+++ src/main/java/ie104/doAn/dao/TinTucRepo.java
@@ -1,9 +1,14 @@
 package ie104.doAn.dao;
 
 import ie104.doAn.model.TinTuc;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 
 @RepositoryRestResource
 public interface TinTucRepo extends JpaRepository<TinTuc, Integer> {
+    Page<TinTuc> findByTieuDeContainingIgnoreCase(@Param("tieuDe") String tieuDe, Pageable pageable);
 }
